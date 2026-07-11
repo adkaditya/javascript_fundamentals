5511b92bc8e3de40d3ae3203124b9119d4361094
Aditya Kumar <adk6535@gmail.com>
2026-07-11 16:50:20 +0000

    variables

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,29 @@
+const accountId = 144553
+let accountEmail = "adk6535@gmail.com"
+ accountCity = "Jaipur"
+ /* javascript ek aise safe programing language hai jo 
+  bina variable ke aage kuch bhi aap ek variable ki memory rserve kar sakte ho*/
+
+ let accountState;
+
+
+ // accountId = // not allowed
+
+ accountEmail = "adkjd244@gmail.com"
+ accountPassword = "4435432"
+ accountCity = "Lucknow"
+
+
+ console.log(accountId);
+
+ /*
+
+
+ Prefer not to use var
+ because of issue in block scope and functional scope
+
+ */
+
+
+ console.table([accountId , accountEmail, accountPassword , accountcity])
+
